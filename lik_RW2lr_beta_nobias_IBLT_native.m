2f9e6773f7c0796d66d9608ad4e6190663150539
EddieBWXiao <81960460+EddieBWXiao@users.noreply.github.com>
2022-02-26 19:47:26 +0000

    Add files via upload

---
+++ lik_RW2lr_beta_nobias_IBLT_native.m
@@ -0,0 +1,70 @@
+function [negLL,vv,pchoice] = lik_RW2lr_beta_nobias_IBLT_native(parameters, actions, opt1)
+
+% likelihood function for given parameters and performance
+% native space
+% fit over only one block (e.g., 60 trials)
+
+% actions: 1 for chosen opt 1 and 2 for chosen opt 2; assume no missing trials
+% opt1: outcomes from only opt1 (see simu_RW2lr_nobias)
+
+%% unpack parameters 
+%alpha1 is alpha-win
+alpha_w  = parameters(1); 
+alpha_l  = parameters(2);
+beta  = parameters(3);
+
+%fixed params
+initial_win_belief = 0.5;
+initial_loss_belief = 0.5;
+
+%% unpack data & preallocate
+wins = opt1(:,1); % 1 for win on trial, 0 for no win
+losses = opt1(:,2); % 1 for loss on trial
+    % note: this is different from the wins & losses used for calc_
+% number of trials
+T = length(wins);
+
+% preallocate traj for opt1
+WPE = nan(size(wins));%updates expectation for wins, for option1
+LPE = nan(size(losses));%updates expectation for losses
+v_w = nan(size(wins));%value learnt from wins (rewards)
+v_l =nan(size(losses));%value learnt from losses (punishments)
+
+pchoice = nan(T,2); % for both options
+p = nan(T,1); % for the chosen option (to calculate L)
+    %one column (p of that choice, on that trial)
+    
+for t=1:T    
+    %% initialise  expectations
+    if t == 1
+        v_w(t) = initial_win_belief;
+        v_l(t) = initial_loss_belief;
+    end
+    %% response model
+    p1 = 1./(1+exp(-beta*(v_w(t)-v_l(t)))); % probability of action 1
+    p2 = 1-p1; % probability of action 2
+    pchoice(t,:) = [p1,p2]; %record both
+    
+    % get probability of the chosen action
+    if actions(t) == 1
+        p(t) = p1;
+    elseif actions(t) ==2
+        p(t) = p2;
+    end   
+    
+    %% learning (only about option 1)
+    WPE(t) = wins(t) - v_w(t);%calculate PE
+    LPE(t) = losses(t) - v_l(t);
+    v_w(t+1) = v_w(t) + alpha_w*WPE(t); %update value
+    v_l(t+1) = v_l(t) + alpha_l*LPE(t); 
+    
+end
+
+vv = [v_w(1:end-1),v_l(1:end-1)]; %output for the two value trajectories
+    %chop off the extra row
+
+% sum of log-probability(data | parameters)
+loglik = sum(log(p+eps));
+negLL = -loglik;
+
+end
